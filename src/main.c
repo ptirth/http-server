44f26b6f2e0bb941c60c5865f6c8d66d48bd2468
Tirth Patel <tirthpatel61@outlook.com>
2024-10-26 10:50:43 +0530

    chore: Format the source code

--- src/main.c
+++ src/main.c
@@ -45,7 +45,8 @@ int main(int argc, char *argv[]) {
   struct sockaddr_in client_addr;
   socklen_t client_addr_len = sizeof(client_addr);
 
-  int connection_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+  int connection_fd =
+      accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
   if (connection_fd == -1) {
     printf("Connection with client failed: %s \n", strerror(errno));
     return 1;
