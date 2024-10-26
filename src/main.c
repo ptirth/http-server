0c1da4a3ae9f7a15f0ba52c5e9b9e6cc25d43a3f
Tirth Patel <tirthpatel61@outlook.com>
2024-10-26 08:54:07 +0530

    Open server socket and wait for client request

--- src/main.c
+++ src/main.c
@@ -1,4 +1,58 @@
-int main(int argc, char* argv[]) {
-  // TODO: Start a server socket and wait for client request
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define SERVER_PORT 8080
+
+int main(int argc, char *argv[]) {
+  // Disable output buffering
+  setbuf(stdout, NULL);
+  setbuf(stderr, NULL);
+
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd == -1) {
+    printf("Socket creation failed: %s...\n", strerror(errno));
+    return 1;
+  }
+
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
+      0) {
+    printf("SO_REUSEADDR failed: %s \n", strerror(errno));
+    return 1;
+  }
+
+  struct sockaddr_in serv_addr = {
+      .sin_family = AF_INET,
+      .sin_port = htons(SERVER_PORT),
+      .sin_addr = {htonl(INADDR_ANY)},
+  };
+
+  if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
+    printf("Bind failed: %s \n", strerror(errno));
+    return 1;
+  }
+
+  int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0) {
+    printf("Listen failed: %s \n", strerror(errno));
+    return 1;
+  }
+
+  printf("Waiting for a client to connect...\n");
+
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+
+  accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+  printf("Client connected\n");
+
+  close(server_fd);
+
   return 0;
 }
